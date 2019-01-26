706b8d5313a2ad59f7d3164c856ec6eb210afa86
thomas367 <thomas.367@hotmail.com>
2019-01-26 18:51:58 +0200

    First part of portfolio site

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {	
+	
+	$(function(){
+		new TypeIt('.welcome-box p',{
+			speed:70,
+			cursorSpeed:800
+		});	
+		
+	});
+	
+	$('.welcome-box h1').animate({opacity: 1}, 2000, function() {	
+	});
+	
+	
+});
